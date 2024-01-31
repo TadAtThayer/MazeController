7ae36fb2a93005e7a466598f2abe92438342c765
Tad Truex <tad@dartmouth.edu>
2024-01-30 20:02:35 -0500

    add a DNP to the schematic.  Step/Dir works OK

--- Code/Workspace/StepperFW/Core/Src/main.c
+++ Code/Workspace/StepperFW/Core/Src/main.c
@@ -166,7 +166,7 @@ static void MX_I2C1_Init(void)
   /* USER CODE END I2C1_Init 1 */
   hi2c1.Instance = I2C1;
   hi2c1.Init.Timing = 0x40000A0B;
-  hi2c1.Init.OwnAddress1 = 64;
+  hi2c1.Init.OwnAddress1 = 66;
   hi2c1.Init.AddressingMode = I2C_ADDRESSINGMODE_7BIT;
   hi2c1.Init.DualAddressMode = I2C_DUALADDRESS_DISABLE;
   hi2c1.Init.OwnAddress2 = 0;
